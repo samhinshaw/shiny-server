1191bac8efa09e41274948280ecebab617269d1d
Sam Hinshaw <samuel.hinshaw@gmail.com>
2015-12-09 20:47:56 -0800

    Adding files for shiny app!

---
+++ server.R
@@ -0,0 +1,106 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(scales)
+url <- url("http://pub.data.gov.bc.ca/datasets/176284/BC_Liquor_Store_Product_Price_List.csv")
+bcl <- read.csv(url, stringsAsFactors = FALSE)
+products <- c("BEER", "REFRESHMENT BEVERAGE", "SPIRITS", "WINE")
+bcl <- dplyr::filter(bcl, PRODUCT_CLASS_NAME %in% products) %>%
+	dplyr::select(-PRODUCT_TYPE_NAME, -PRODUCT_SKU_NO, -PRODUCT_BASE_UPC_NO,
+								-PRODUCT_SUB_CLASS_NAME) %>%
+	rename(Type = PRODUCT_CLASS_NAME,
+				 Subtype = PRODUCT_MINOR_CLASS_NAME,
+				 Name = PRODUCT_LONG_NAME,
+				 Country = PRODUCT_COUNTRY_ORIGIN_NAME,
+				 Alcohol_Content = PRODUCT_ALCOHOL_PERCENT,
+				 Price = CURRENT_DISPLAY_PRICE,
+				 Sweetness = SWEETNESS_CODE, 
+				 Liters_Per_Unit = PRODUCT_LITRES_PER_CONTAINER, 
+				 Count = PRD_CONTAINER_PER_SELL_UNIT)
+bcl$Type <- sub("^REFRESHMENT BEVERAGE$", "REFRESHMENT", bcl$Type)
+function(input, output, session) {
+	output$prettysweetplot <- renderPlot({
+		checkrows <- lapply(input, FUN = nrow)
+		if(0 %in% checkrows){
+			return(NULL)
+		}
+		if(input$expensive == "No"){
+			filtered <-
+				bcl %>% 
+				filter(Alcohol_Content >= input$alcoholInput[1],
+							 Alcohol_Content <= input$alcoholInput[2],
+							 Price >= input$priceInput2[1],
+							 Price <= input$priceInput2[2],
+							 Type == input$typeInput,
+							 Country == input$countryInput)
+		} else if(input$expensive == "Yes") {
+			filtered <-
+				bcl %>% 
+				filter(Alcohol_Content >= input$alcoholInput[1],
+							 Alcohol_Content <= input$alcoholInput[2],
+							 Price >= input$priceInput[1],
+							 Price <= input$priceInput[2],
+							 Type == input$typeInput,
+							 Country == input$countryInput)
+		}
+		tetrad <- c("BEER" = '#B260AF', "WINE" = '#1DADFF',"SPIRITS" = '#FFD844', "REFRESHMENT" = '#FF6715')
+		ggplot(filtered, aes(Price, fill = Type)) +
+			geom_histogram(color="black", binwidth = 1) +
+			xlab("Price") +
+			ylab("Frequency") +
+			ggtitle("Frequency Distribution of Price") +
+			scale_fill_manual(name = "Beverage Type", values = tetrad) +
+			scale_x_continuous(labels = dollar)
+		
+	})
+	output$results <- DT::renderDataTable({
+		if(input$expensive == "No"){
+			if(input$sortup == "Ascending"){
+				filtered <-
+					bcl %>% 
+					arrange_(input$sortby) %>%
+					filter(Alcohol_Content >= input$alcoholInput[1],
+								 Alcohol_Content <= input$alcoholInput[2],
+								 Price >= input$priceInput2[1],
+								 Price <= input$priceInput2[2],
+								 Type == input$typeInput,
+								 Country == input$countryInput)
+			}
+			else if(input$sortup == "Descending"){
+				filtered <-
+					bcl %>% 
+					arrange_(paste0(c('desc(', input$sortby, ')'))) %>%
+					filter(Alcohol_Content >= input$alcoholInput[1],
+								 Alcohol_Content <= input$alcoholInput[2],
+								 Price >= input$priceInput2[1],
+								 Price <= input$priceInput2[2],
+								 Type == input$typeInput,
+								 Country == input$countryInput)
+			}
+		} else if(input$expensive == "Yes"){
+			if(input$sortup == "Ascending"){
+				filtered <-
+					bcl %>% 
+					arrange_(input$sortby) %>%
+					filter(Alcohol_Content >= input$alcoholInput[1],
+								 Alcohol_Content <= input$alcoholInput[2],
+								 Price >= input$priceInput[1],
+								 Price <= input$priceInput[2],
+								 Type == input$typeInput,
+								 Country == input$countryInput)
+			}
+			else if(input$sortup == "Descending"){
+				filtered <-
+					bcl %>% 
+					arrange_(paste0(c('desc(', input$sortby, ')'))) %>%
+					filter(Alcohol_Content >= input$alcoholInput[1],
+								 Alcohol_Content <= input$alcoholInput[2],
+								 Price >= input$priceInput[1],
+								 Price <= input$priceInput[2],
+								 Type == input$typeInput,
+								 Country == input$countryInput)
+			}
+		}
+		filtered
+	})
+}
---
+++ ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(shinyjs)
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+fluidPage(theme = "bootstrap.css",
+					useShinyjs(),
+	titlePanel(img(src = "logo_bc_liquor_stores.png")),
+	h4("\"Filter Yo\' Beverage!\""),
+	sidebarLayout(
+		sidebarPanel(
+			sliderInput("alcoholInput", "Choose an Alcohol Content Range",
+									min = 0, max = 100, value = c(0, 20), post = "%"),
+			conditionalPanel(condition = "input.expensive == 'Yes'",
+											 sliderInput("priceInput", "Choose a Price Range",
+											 						min = 0, max = 2000, value = c(20, 40), pre = "$")),
+			conditionalPanel(condition = "input.expensive == 'No'",
+											 sliderInput("priceInput2", "Choose a Price Range",
+											 						min = 0, max = 100, value = c(20, 40), pre = "$")),
+			selectInput("expensive", "Allow Expensive Options?", choices = c("Yes", "No"), selected = "No"),
+			checkboxGroupInput("typeInput", "Types of Beverages", 
+												 choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"), 
+												 selected = c("BEER", "REFRESHMENT", "SPIRITS", "WINE")), 
+			checkboxGroupInput("countryInput", "Country",
+									choices = c("CANADA", "FRANCE", "ITALY"), 
+									selected = c("CANADA", "FRANCE", "ITALY")),
+			h4(em(strong("Sort Table"), "(or use header)")),
+			selectInput("sortby", "Sort By", choices = c("Price", "Alcohol_Content", "Sweetness"), selected = "Price"),
+			selectInput("sortup", "Ascending or Descending?", choices = c("Ascending", "Descending"), selected = "Ascending")
+		),
+		mainPanel(
+			plotOutput("prettysweetplot"), 
+			br(), br(),
+			DT::dataTableOutput("results"), 
+			hr(),
+			"CSS Theme \"Cosmo\" by", a("Thomas Park", href="http://github.com/thomaspark", target="_blank"), "at", a("bootswatch.com", href="http://bootswatch.com", target="_blank")
+			
+		)
+	)
+)
