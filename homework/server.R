000cc0bd0104dbba37f0393e700a0e93fa1c7af0
Sam Hinshaw <samuel.hinshaw@gmail.com>
2015-12-09 23:16:02 -0800

    changed download

--- homework/server.R
+++ homework/server.R
@@ -2,22 +2,23 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(scales)
-url <- url("http://pub.data.gov.bc.ca/datasets/176284/BC_Liquor_Store_Product_Price_List.csv")
-bcl <- read.csv(url, stringsAsFactors = FALSE)
-products <- c("BEER", "REFRESHMENT BEVERAGE", "SPIRITS", "WINE")
-bcl <- dplyr::filter(bcl, PRODUCT_CLASS_NAME %in% products) %>%
-	dplyr::select(-PRODUCT_TYPE_NAME, -PRODUCT_SKU_NO, -PRODUCT_BASE_UPC_NO,
-								-PRODUCT_SUB_CLASS_NAME) %>%
-	rename(Type = PRODUCT_CLASS_NAME,
-				 Subtype = PRODUCT_MINOR_CLASS_NAME,
-				 Name = PRODUCT_LONG_NAME,
-				 Country = PRODUCT_COUNTRY_ORIGIN_NAME,
-				 Alcohol_Content = PRODUCT_ALCOHOL_PERCENT,
-				 Price = CURRENT_DISPLAY_PRICE,
-				 Sweetness = SWEETNESS_CODE, 
-				 Liters_Per_Unit = PRODUCT_LITRES_PER_CONTAINER, 
-				 Count = PRD_CONTAINER_PER_SELL_UNIT)
-bcl$Type <- sub("^REFRESHMENT BEVERAGE$", "REFRESHMENT", bcl$Type)
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+# url <- url("http://pub.data.gov.bc.ca/datasets/176284/BC_Liquor_Store_Product_Price_List.csv")
+# bcl <- read.csv(url, stringsAsFactors = FALSE)
+# products <- c("BEER", "REFRESHMENT BEVERAGE", "SPIRITS", "WINE")
+# bcl <- dplyr::filter(bcl, PRODUCT_CLASS_NAME %in% products) %>%
+# 	dplyr::select(-PRODUCT_TYPE_NAME, -PRODUCT_SKU_NO, -PRODUCT_BASE_UPC_NO,
+# 								-PRODUCT_SUB_CLASS_NAME) %>%
+# 	rename(Type = PRODUCT_CLASS_NAME,
+# 				 Subtype = PRODUCT_MINOR_CLASS_NAME,
+# 				 Name = PRODUCT_LONG_NAME,
+# 				 Country = PRODUCT_COUNTRY_ORIGIN_NAME,
+# 				 Alcohol_Content = PRODUCT_ALCOHOL_PERCENT,
+# 				 Price = CURRENT_DISPLAY_PRICE,
+# 				 Sweetness = SWEETNESS_CODE, 
+# 				 Liters_Per_Unit = PRODUCT_LITRES_PER_CONTAINER, 
+# 				 Count = PRD_CONTAINER_PER_SELL_UNIT)
+# bcl$Type <- sub("^REFRESHMENT BEVERAGE$", "REFRESHMENT", bcl$Type)
 function(input, output, session) {
 	output$prettysweetplot <- renderPlot({
 		checkrows <- lapply(input, FUN = nrow)
